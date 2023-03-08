dd73f83a04657c83eac4d14fab6c08f46b4c9b5b
Sanket Koli <kolisanket15@gmail.com>
2023-03-08 13:35:00 -0800

    SVG element attribute rename

--- src/components/Footer/SocialMediaLinks.js
+++ src/components/Footer/SocialMediaLinks.js
@@ -29,22 +29,22 @@ const SocialMediaLinks = (props) => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 {" "}
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M12.0004 8C20.3886 8.00052 21.7129 10.6594 21.8897 11.7802C21.9329 11.9459 22.6286 15.6383 19.8986 15.9247C13.1064 16.6189 17.7759 11.9168 11.9997 12.0954C6.22345 12.274 10.8928 16.6189 4.10194 15.925C1.37129 15.638 2.06709 11.9456 2.11033 11.7813C2.28654 10.6598 3.61158 8.00016 12.0004 8Z"
                   stroke="#ffffff"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>{" "}
               </g>
             </svg>
@@ -80,11 +80,11 @@ const SocialMediaLinks = (props) => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 {" "}
