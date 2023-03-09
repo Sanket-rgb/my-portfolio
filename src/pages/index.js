58f21052d85bbdc621d245d548e45036282c9508
Sanket Koli <kolisanket15@gmail.com>
2023-03-08 19:47:55 -0800

    Highlight Nav item

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import React, { useState } from "react"
 import About from "@/components/About/About"
 import Hobbies from "@/components/Hobbies/Hobbies"
 import Work from "@/components/Work/Work"
-import classes from "../styles/Home.module.css"
+
 import WorkInfo from "@/components/Work/WorkInfo"
 import HobbiesInfo from "@/components/Hobbies/HobbiesInfo"
 import AboutInfo from "@/components/About/AboutInfo"
+
 import SocialMediaLinks from "@/components/Footer/SocialMediaLinks"
+
+import classes from "../components/Navigation.module.css"
 
 export default function Home() {
   const [selectedNav, setSelectedNav] = useState("ABOUT")
@@ -18,17 +21,11 @@ export default function Home() {
 
   return (
     <React.Fragment>
-      <ul className={classes.nav}>
-        <li>
-          <Work onSelectNav={navSelectorhandler} />
-        </li>
-        <li>
-          <Hobbies onSelectNav={navSelectorhandler} />
-        </li>
-        <li>
-          <About onSelectNav={navSelectorhandler} />
-        </li>
-      </ul>
+      <div className={classes["nav-bar"]}>
+        <Work selectedNav={selectedNav} onSelectNav={navSelectorhandler} />
+        <Hobbies selectedNav={selectedNav} onSelectNav={navSelectorhandler} />
+        <About selectedNav={selectedNav} onSelectNav={navSelectorhandler} />
+      </div>
       {selectedNav === "WORK" && <WorkInfo />}
       {selectedNav === "HOBBIES" && <HobbiesInfo />}
       {selectedNav === "ABOUT" && <AboutInfo />}
