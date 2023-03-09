c4d18766a9724c6ad960fa33711c414665859cd5
Sanket Koli <kolisanket15@gmail.com>
2023-03-08 19:48:48 -0800

    Improved UI for About Nav item

--- src/components/About/AboutInfo.js
+++ src/components/About/AboutInfo.js
@@ -2,45 +2,57 @@ import React from "react"
 
 import Image from "next/image"
 import myself from "../../../public/Images/Photographs/Myself.jpeg"
-import Card from "../UI/Card"
 import classes from "./AboutInfo.module.css"
 
 const AboutInfo = (props) => {
   return (
-    <div className={classes.layout}>
-      <div className={classes.imageContainer}>
+    <div className={classes["grid-container"]}>
+      <div className={classes["image-container"]}>
         <Image
           fill
           alt="self"
           src={myself}
           className={classes.image}
           placeholder="blur"
+          sizes="(max-width: 768px) 100vw,
+          (max-width: 1200px) 50vw,
+          33vw"
         />
       </div>
+      <div className={classes["info-details"]}>
+        <h1>Sanket Koli | Software Engineer, Photographer</h1>
+        <p>
+          Experienced Frontend Developer with 2 Years of Expertise in Building
+          User-Friendly Web/Mobile Interfaces.
+        </p>
+        <p>
+          I possess a proven track record of spearheading front-end development
+          initiatives at a thriving startup, driving organizational growth and
+          success through effective leadership and seamless client interactions.
+        </p>
+        <p>
+          Adept in building visually appealing and highly functional user
+          interfaces, with a strong focus on user experience and accessibility.
+        </p>
+        <h1>Skills</h1>
 
-      <div className={classes.content}>
-        <div>
-          <h1>Sanket Koli | Photographer - Software Engineer</h1>
-          <p className={classes.p}>
-            I am a graduate student in computer science with a Masters degree
-            from Pace University and a Postgraduate Diploma in Advanced
-            Computing from MET IIT, CDAC.I am a graduate student in computer
-            science with a Masters degree from Pace University and a
-            Postgraduate Diploma in Advanced Computing from MET IIT, CDAC.
-          </p>
-        </div>
+        <ul>
+          <li>Web/Mobile Technology: HTML, CSS, ReactJS, Flutter</li>
+        </ul>
+        <ul>
+          <li>Programming languages: Python, JavaScript, C</li>
+        </ul>
+        <br />
         <h1>Education</h1>
-        <div>
-          <p className={classes.p}>
-            MS in Computer Science at Pace University, NYC
-          </p>
-          <p className={classes.p}>
-            Diploma in Advanced Computing at CDAC, MUMBAI
-          </p>
-          <p className={classes.p}>
-            BE in Instrumentation Engineering, Mumbai University
-          </p>
-        </div>
+        <ul>
+          <li>Masters in Computer Science, New York City</li>
+        </ul>
+        <ul>
+          <li>Diploma in Advanced Computing, Mumbai</li>
+        </ul>
+        <ul>
+          <li>Bachelors in Instrumentation Engineering, Mumbai</li>
+        </ul>
       </div>
     </div>
   )
