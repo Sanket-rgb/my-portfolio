b61feb8c19f722a553c1b1ecef6b097e6a2360ad
Sanket Koli <kolisanket15@gmail.com>
2023-03-29 17:27:42 -0700

    Added favicon and page meta-data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,37 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import About from "@/components/About/About";
-import Hobbies from "@/components/Hobbies/Hobbies";
-import Work from "@/components/Work/Work";
+import About from "@/components/About/About"
+import Hobbies from "@/components/Hobbies/Hobbies"
+import Work from "@/components/Work/Work"
 
-import WorkInfo from "@/components/Work/WorkInfo";
-import HobbiesInfo from "@/components/Hobbies/HobbiesInfo";
-import AboutInfo from "@/components/About/AboutInfo";
+import WorkInfo from "@/components/Work/WorkInfo"
+import HobbiesInfo from "@/components/Hobbies/HobbiesInfo"
+import AboutInfo from "@/components/About/AboutInfo"
 
-import SocialMediaLinks from "@/components/Footer/SocialMediaLinks";
+import SocialMediaLinks from "@/components/Footer/SocialMediaLinks"
 
-import classes from "../components/Navigation.module.css";
-import Image from "next/image";
+import classes from "../components/Navigation.module.css"
+import Image from "next/image"
+import Head from "next/head"
 
 export default function Home() {
-  const [selectedNav, setSelectedNav] = useState("ABOUT");
-  const [menuList, setMenuList] = useState(false);
+  const [selectedNav, setSelectedNav] = useState("ABOUT")
+  const [menuList, setMenuList] = useState(false)
 
   const navSelectorhandler = (selectedNav) => {
-    setSelectedNav(selectedNav);
-  };
+    setSelectedNav(selectedNav)
+  }
 
   const menuListHandler = () => {
-    setMenuList(!menuList);
-  };
+    setMenuList(!menuList)
+  }
 
   return (
     <React.Fragment>
+      <Head>
+        <title>Sanket Koli | Front-end Developer</title>
+        <meta name="about" content="About me" key="desc" />
+      </Head>
       <header className={classes["nav-bar"]}>
         <div className={classes["nav-bar-items"]}>
           <Work selectedNav={selectedNav} onSelectNav={navSelectorhandler} />
@@ -69,5 +74,5 @@ export default function Home() {
       {selectedNav === "ABOUT" && <AboutInfo />}
       <SocialMediaLinks tabType={selectedNav} />
     </React.Fragment>
-  );
+  )
 }
